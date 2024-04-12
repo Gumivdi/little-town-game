c00a95ebcaa71c852cc9c23145bd0358bbb17741
Gimmy van Deurse <gimmyvandeurse@gmail.com>
2024-04-13 00:51:41 +0200

    [feature/data] Setup

---
+++ src/data/setup.data.ts
@@ -0,0 +1,27 @@
+import { TSetup } from "shared/types/setup.type";
+
+const TWO_PLAYERS: TSetup = {
+  workers: 5,
+  buildings: 7,
+  resources: {
+    coin: 3,
+  },
+};
+
+const THREE_PLAYERS: TSetup = {
+  workers: 4,
+  buildings: 6,
+  resources: {
+    coin: 3,
+  },
+};
+
+const FOUR_PLAYERS: TSetup = {
+  workers: 3,
+  buildings: 6,
+  resources: {
+    coin: 3,
+  },
+};
+
+export const DSetup = [TWO_PLAYERS, THREE_PLAYERS, FOUR_PLAYERS];
