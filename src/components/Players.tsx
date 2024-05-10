88bfa2d280c59ec16379855c73d9bac8dd373bc8
Gimmy van Deurse <gimmyvandeurse@gmail.com>
2024-05-10 21:48:19 +0200

    [feature/setup] Players context

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,10 +1,13 @@
-import { DPlayers } from "@/data/players.data";
+import { useContext } from "react";
+import { PlayersContext } from "@/context/players.context";
 import Player from "@/components/Player/Player";
 
 const Players = () => {
+  const { players } = useContext(PlayersContext);
+
   return (
     <ul className="grid gap-2">
-      {DPlayers.map((player) => {
+      {players.map((player) => {
         const playerKey = `player-${player.id}`;
         return <Player key={playerKey} data={player} />;
       })}
