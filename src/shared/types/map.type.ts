929e41e7d97e91a56ab3fdc38dba11ca45e41601
Gimmy van Deurse <gimmyvandeurse@gmail.com>
2024-05-17 12:08:12 +0200

    [feature/action-play-worker] Extend map type

--- src/shared/types/map.type.ts
+++ src/shared/types/map.type.ts
@@ -2,10 +2,14 @@ import { ETerrains } from "@/shared/enums/terrains.enum";
 import { TBuilding } from "./building.type";
 
 export type TLandscapeArea = {
+  id?: string;
+  disabled?: boolean;
   type: Exclude<ETerrains, ETerrains.GRASS>;
 };
 
 export type TBuildArea = {
+  id?: string;
+  disabled?: boolean;
   type: Required<ETerrains.GRASS>;
   building: TBuilding | null;
   owner: number | null;
